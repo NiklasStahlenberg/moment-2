075235b591a55056b2e60bd2ce1a81c3728d10dd
Niklas Ståhlenberg <Stais3r@gmail.com>
2019-09-17 20:47:03 +0200

    code added in gulpfile.js, copy css and html

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,22 @@
-const {src, dest} = require("gulp");
+const {src, dest, watch, series, parallel} = require("gulp");
 
+const files = {
+    htmlPath: "src/**/*.html",
+    cssPath: "src/styles/*.css"
+}
+
+function copyHTML(){
+    return src(files.htmlPath)
+        .pipe(dest('pub')
+    );
+}
+
+function copyCss(){
+    return src(files.cssPath)
+        .pipe(dest('pub/css')
+    );
+}
+
+
+
+exports.default = series(copyHTML, copyCss);
